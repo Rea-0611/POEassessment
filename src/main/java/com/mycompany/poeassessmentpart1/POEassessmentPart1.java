460a8d448f479661d119145b7f3198657a3fc688
Rea-0611 <ST10483202@rcconnect.edu.za>
2025-04-10 08:42:03 +0000

    Initial commit

---
+++ src/main/java/com/mycompany/poeassessmentpart1/POEassessmentPart1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poeassessmentpart1;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class POEassessmentPart1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
